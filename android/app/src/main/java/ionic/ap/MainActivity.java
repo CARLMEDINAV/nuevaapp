22d36b07987a04c343286a067a3c9e95f35935c4
CARLMEDINAV <carl.medinav@duocuc.cl>
2024-09-17 22:36:30 -0300

    Initial commit

---
+++ android/app/src/main/java/ionic/ap/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.ap;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.ap',
+  appName: 'ap',
+  webDir: 'www'
+};
+
+export default config;
